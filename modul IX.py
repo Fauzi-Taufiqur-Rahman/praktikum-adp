4154af840e579bd9db2f6c6939be105fbe89efc9
Fauzi-Taufiqur-Rahman <fauzitaufiqurrahman8@gmail.com>
2025-06-10 12:45:02 +0700

    Add files via upload

--- modul IX.py
+++ modul IX.py
@@ -2,47 +2,53 @@ from os import system as clear
 from time import sleep
 from termcolor import cprint
 
-# Logo Roblox versi ASCII (lubang tengah bergaya diamond)
 roblox_logo = [
-    "        #######        ",
-    "      ###########      ",
-    "     ####     ####     ",
-    "    ####       ####    ",
-    "   ####         ####   ",
-    "  ####           ####  ",
-    " ####             #### ",
-    "####               ####",
-    " ####             #### ",
-    "  ####           ####  ",
-    "   ####         ####   ",
-    "    ####       ####    ",
-    "     ####     ####     ",
-    "      ###########      ",
-    "        #######        ",
+    "                                                               ",
+    "                     ##                                        ",
+    "                     ######                                    ",
+    "                    ###########                                ",
+    "                    ###############                            ",       
+    "                   ####################                        ",
+    "                   ########################                    ",
+    "                  #############################                ",            
+    "                  #################################            ",
+    "                 ##############  #######################       ",
+    "                 #############      ###################        ",
+    "                #############          ###############         ",
+    "                ############              ############         ",
+    "               ############              ############          ",
+    "               ###############          #############          ",
+    "              ###################      #############           ",
+    "              ######################  ##############           ",
+    "                  #################################            ",
+    "                      #############################            ",
+    "                          ########################             ",
+    "                              ####################             ",
+    "                                  ###############              ",
+    "                                      ###########              ",
+    "                                          ######               ",
+    "                                              ##               ",
 ]
 
-# Bersihkan layar
-clear("cls")
+clear('cls')
 
-# Tampilkan logo baris per baris (animasi masuk)
-for row in roblox_logo:
-    for char in row:
-        if char == "#":
-            cprint(" ", on_color="on_red", end="")
+for baris in roblox_logo:
+    for karakter in baris:
+        if karakter == "#":
+            cprint(" ", on_color="on_white", end="")
         else:
             print(" ", end="")
+        sleep(0.002)  
     print()
-    sleep(0.1)
 
-# Loading muncul di bawah logo
+ 
 for i in range(101):
-    print(' ' * 20, end='')  # indentasi
-    cprint(f"Loading...{i}%", "red", attrs=['bold'], end='\r')
-    sleep(0.05)
+    print(' ' * 28, end='')   
+    cprint(f"Loading...{i}%", "white", attrs=['bold'], end='\r')
+    sleep(0.03)
 
-# Pindah baris agar loading tidak tertimpa
+
 print()
-cprint("Berhasil Memuat Game!".center(70), "white", "on_red", attrs=['bold'])
 
-sleep(2)
-clear("cls")
+cprint("Berhasil Memuat Game!".center(70), "white", "on_white", attrs=["bold"])
+
